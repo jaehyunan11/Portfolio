d8ad5e5ffc61cf57f22d8524b3f7b9c630b8f1eb
jaehyun An <jaehyuna11@gmail.com>
2020-12-28 22:56:04 -0800

    javascript home contact button

--- main.js
+++ main.js
@@ -25,7 +25,18 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
     console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView();
+    scrollIntoView(link)
 
 });
+
+// Handle click on "contact me" button on home
+const homeContact = document.querySelector('.home__contact');
+homeContact.addEventListener('click', (event) => {
+    scrollIntoView('#contact')
+})
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+
+}
